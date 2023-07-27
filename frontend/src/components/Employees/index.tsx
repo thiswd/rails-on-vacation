315ad0601db2ae33e85bb1035b82c97a014b6faa
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-27 17:32:02 -0300

    feat: 📅 Added a date range picker to the Employees component
    This commit integrates the 'react-date-range' library into the Employees component of the frontend React application. The purpose is to allow the user to pick a date range.
    
    The main changes in the code include:
    - Imported necessary items from the 'react-date-range' library
    - Set default properties for the date picker including locale, minDate, and date format
    - Added the 'DateRange' component to the render method of the Employees component
    - Handled state changes for the date range using the 'useState' React Hook

--- frontend/src/components/Employees/index.tsx
+++ frontend/src/components/Employees/index.tsx
@@ -1,5 +1,38 @@
-import { EmployeesList } from "./EmployeesList"
+import { DateRange, Range } from "react-date-range"
 
+import { EmployeesList } from "./EmployeesList"
+import "react-date-range/dist/styles.css" // main css file
+import "react-date-range/dist/theme/default.css" // theme css file
+import { useState } from "react"
+import { pt } from "date-fns/locale"
+
+const minDate = new Date()
+const dateFormat = "d MMMM yyyy"
+const endDateText = "Data final"
+const initialRange = {
+  startDate: minDate,
+  endDate: undefined,
+  key: "selection",
+}
 export function Employees() {
-  return <EmployeesList></EmployeesList>
+  const [range, setRange] = useState<Range[]>([initialRange])
+
+  return (
+    <div className="w-[1024px] h-screen mx-auto flex flex-col items-center">
+      <EmployeesList />
+      <div className="w-full h-full flex vle items-center justify-center">
+        <DateRange
+          editableDateInputs={true}
+          onChange={item => setRange([item.selection])}
+          ranges={range}
+          months={2}
+          direction="horizontal"
+          minDate={minDate}
+          locale={pt}
+          dateDisplayFormat={dateFormat}
+          endDatePlaceholder={endDateText}
+        />
+      </div>
+    </div>
+  )
 }
