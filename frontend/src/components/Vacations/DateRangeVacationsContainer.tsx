95f6eb80c432a18172b81a1d7237f2a3bc9f1e73
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-28 16:54:42 -0300

    feat: enhance employee and vacation UI, add error handling
    - In Employees component, added useEmployee hook to show selected employee details including vacations.
    
    - In DateRangeVacationsContainer, added loading state and error handling for vacation scheduling. Also replaced useRef with useState.
    
    - Created new Spinner component to be displayed during loading states.
    
    - Extended handleDates utility to include checking if a date falls within a certain interval.
    
    - Adjusted typing and formatting across multiple files.

--- frontend/src/components/Vacations/DateRangeVacationsContainer.tsx
+++ frontend/src/components/Vacations/DateRangeVacationsContainer.tsx
@@ -2,11 +2,12 @@ import { DateRange, Range, RangeKeyDict } from "react-date-range"
 
 import "react-date-range/dist/styles.css"
 import "react-date-range/dist/theme/default.css"
-import { FormEvent, useRef, useState } from "react"
+import { FormEvent, useState } from "react"
 import { pt } from "date-fns/locale"
 import { differenceInDays, differenceInMonths, subDays } from "date-fns"
 import { useEmployee } from "../../hooks/useEmployee"
-import { createVacation } from "../../services/api"
+import { VacationType, createVacation } from "../../services/api"
+import { Spinner } from "../shared/Spinner"
 
 const minDate = new Date()
 const yesterday = subDays(minDate, 1) // Start with invalid date
@@ -20,8 +21,11 @@ const initialRange = {
   endDate: yesterday,
   key: "selection",
 }
+
 export function DateRangeVacationsContainer() {
   const [range, setRange] = useState<Range[]>([initialRange])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string>("")
 
   const { selectedEmployee } = useEmployee()
 
@@ -37,51 +41,75 @@ export function DateRangeVacationsContainer() {
     const afterHiring =
       differenceInMonths(startDate, hiring_date) >= yearInMonths
 
+    if (!afterHiring) {
+      const message =
+        "Funcionário só poderá tirar férias um ano depois de sua contratação"
+      setError(message)
+    }
+
     const acceptableDuration =
       differenceInDays(endDate, startDate) >= minDaysDuration
 
-    console.log({ afterHiring, acceptableDuration })
+    if (!acceptableDuration) {
+      const message = "Período de férias não pode ser menor do que 10 dias."
+      setError(message)
+    }
 
     if (afterHiring && acceptableDuration) {
+      setError("")
     }
   }
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
+
+    if (range.length === 0) return
+
+    setLoading(true)
+
     const vacation_data = {
       employee_id: selectedEmployee.id,
       start_date: range[0].startDate,
       end_date: range[0].endDate,
+    } as VacationType
+
+    try {
+      await createVacation(vacation_data)
+    } finally {
+      setLoading(false)
     }
-    console.log(vacation_data)
-    await createVacation(vacation_data)
   }
 
   return (
-    <div className="w-full h-full mb-12">
-      <form onSubmit={handleSubmit} className="flex flex-col">
-        <input type="hidden" name="employeeId" value={selectedEmployee.id} />
+    <div className="relative w-full h-full mb-12">
+      {error && <p className="text-center">{error}</p>}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <input type="hidden" name="employeeId" value={selectedEmployee.id} />
 
-        <DateRange
-          editableDateInputs={true}
-          onChange={handleDateChange}
-          ranges={range}
-          months={2}
-          direction="horizontal"
-          minDate={minDate}
-          locale={pt}
-          dateDisplayFormat={dateFormat}
-          startDatePlaceholder={startDateText}
-          endDatePlaceholder={endDateText}
-        />
+          <DateRange
+            editableDateInputs={true}
+            onChange={handleDateChange}
+            ranges={range}
+            months={2}
+            direction="horizontal"
+            minDate={minDate}
+            locale={pt}
+            dateDisplayFormat={dateFormat}
+            startDatePlaceholder={startDateText}
+            endDatePlaceholder={endDateText}
+          />
 
-        <button
-          type="submit"
-          className="flex items-center justify-center px-10 py-2 -mt-2 text-lg font-light rounded-md bg-neutral-900 text-stone-100 hover:brightness-125 self-end"
-        >
-          Agendar
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="flex items-center justify-center px-10 py-2 -mt-2 text-lg font-light rounded-md bg-neutral-900 text-stone-100 hover:brightness-125 self-end"
+          >
+            Agendar
+          </button>
+        </form>
+      )}
     </div>
   )
 }
